70046279de59a49770c1d8262d49bd8ef65ed714
jied83 <jied83@gmail.com>
2014-10-28 20:34:09 +0400

    wifi: switch from wext to nl80211 driver
    Change-Id: I3851a6be6510ab9057f9467005c318f2f9575a59

--- wifi/board-bcm.mk
+++ wifi/board-bcm.mk
@@ -1,25 +1,16 @@
 # Wifi related defines
 
-BOARD_WPA_SUPPLICANT_DRIVER         := WEXT
-WPA_SUPPLICANT_VERSION              := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB    := lib_driver_cmd_wext
-BOARD_HOSTAPD_DRIVER                := WEXT
-BOARD_HOSTAPD_PRIVATE_LIB           := lib_driver_cmd_wext
-BOARD_WLAN_DEVICE                   := bcm4329
-#BOARD_WLAN_DEVICE_REV              := bcm4329
-BOARD_WLAN_USE_WEXT_DRIVER          := true
-WIFI_BAND                           := 802_11_ABG
-
-WIFI_DRIVER_MODULE_PATH             := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME             := "wlan"
-WIFI_DRIVER_MODULE_ARG              := "firmware_path=/system/etc/wl/bcm43291.bin nvram_path=/system/etc/wl/nvram.txt"
-WIFI_DRIVER_MODULE_AP_ARG           := "firmware_path=/system/etc/wl/bcm43291_apsta.bin nvram_path=/system/etc/wl/nvram.txt"
-WIFI_DRIVER_FW_PATH_PARAM           := "/sys/module/wlan/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA             := "/system/etc/wl/bcm43291.bin"
-WIFI_DRIVER_FW_PATH_AP              := "/system/etc/wl/bcm43291_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P             := "/system/etc/wl/bcm43291_p2p.bin"
-
-WIFI_EXT_MODULE_PATH                := "/system/lib/modules/librasdioif.ko"
-WIFI_EXT_MODULE_NAME                := "librasdioif"
-
 BOARD_HAVE_PRESTO_WIFI              := true
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_BAND := 802_11_ABG
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wl/bcm43291.bin"
+WIFI_DRIVER_FW_PATH_P2P	    := "/system/etc/wl/bcm43291.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wl/bcm43291_apsta.bin"
+WIFI_DRIVER_MODULE_NAME     := "bcmdhd"
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcmdhd.ko"
