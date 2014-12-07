f1e1e3724e64f62d181ddd3391290ac569f6ede9
mifl <mifl.pc@gmail.com>
2014-12-07 19:51:19 +0100

    presto: board-bcm: use android 5.0 style

--- wifi/board-bcm.mk
+++ wifi/board-bcm.mk
@@ -1,16 +1,19 @@
 # Wifi related defines
 
-BOARD_HAVE_PRESTO_WIFI              := true
-BOARD_WLAN_DEVICE := bcmdhd
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 WPA_SUPPLICANT_VERSION := VER_0_8_X
-WIFI_BAND := 802_11_ABG
-WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wl/bcm43291.bin"
-WIFI_DRIVER_FW_PATH_P2P	    := "/system/etc/wl/bcm43291.bin"
-WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wl/bcm43291_apsta.bin"
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_AP := "/system/etc/wl/bcm43291_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/wl/bcm43291.bin"
+WIFI_DRIVER_FW_PATH_P2P := "/system/etc/wl/bcm43291.bin"
+
 WIFI_DRIVER_MODULE_NAME     := "bcmdhd"
 WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcmdhd.ko"
+
+WIFI_BAND := 802_11_ABG
+
+BOARD_HAVE_PRESTO_WIFI := true
